d68636d489aa28ac431ef14434795c54ed733bee
MitchellGleason <111290810+MitchellGleason@users.noreply.github.com>
2022-11-07 23:22:22 -0500

    Update logic.js

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -5,11 +5,26 @@ console.log("working");
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //Create the tile layer that will be the background of our map
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    accessToken: API_KEY
-});
+let streets = L.tileLayer(
+	'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
+	{
+		attribution:
+			'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		accessToken: API_KEY,
+	}
+);
+
+//Create the dark view tile layer that will be an option for our map
+let dark = L.tileLayer(
+	'https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}',
+	{
+		attribution:
+			'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		accessToken: API_KEY,
+	}
+);
 
 //Add our 'graymap' tile layer to the map.
 streets.addTo(map);
